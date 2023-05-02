f997b0e840ae37d0bc6d347a3863e22bd6d9d512
pizzaboyyy <121211168+pizzaboyyy@users.noreply.github.com>
2023-05-03 11:34:54 +1200

    made a function that will show the instructions

---
+++ instructions_v1.py
@@ -0,0 +1,10 @@
+# instructions to show the user how to play the game if they have not played before
+
+# function to display instructions
+def instructions():
+    print("***** How to play ******")
+    print()
+    print("The rules of the game will go here")
+    print()
+    print("Program continues")
+    print()
